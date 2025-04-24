decddd10aef3379d0414ab4a6811bde2928d2bea
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2025-04-24 22:47:44 +0300

    Update env.ts

--- common/env.ts
+++ common/env.ts
@@ -1,7 +1,7 @@
 export const config = {
-  API_URL: "http://localhost:4000/api",
-  MEDIA_URL: "http://localhost:4000",
+  // API_URL: "http://localhost:4000/api",
+  // MEDIA_URL: "http://localhost:4000",
 
-  // API_URL: "https://blow.igoshev.de/api",
-  // MEDIA_URL: "https://blow.igoshev.de"
+  API_URL: "https://blow.igoshev.de/api",
+  MEDIA_URL: "https://blow.igoshev.de"
 };
