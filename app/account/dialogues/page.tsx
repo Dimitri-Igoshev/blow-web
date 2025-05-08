d2921242a0c9d84ea7b8054a326c553806349cad
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-05-08 13:47:49 +0300

    dialogs mobile

--- app/account/dialogues/page.tsx
+++ app/account/dialogues/page.tsx
@@ -3,17 +3,38 @@
 import { Button } from "@heroui/button";
 import { Image } from "@heroui/image";
 import { Input } from "@heroui/input";
+import { cn } from "@heroui/theme";
+import { useState } from "react";
 
 export default function AccountDialogues() {
+	const [isDialog, setIsDialog] = useState(false);
+
 	return (
 		<div className="flex w-full flex-col px-3 md:px-9 pt-[84px] gap-[30px] h-screen">
 			<div className="flex w-full items-center justify-between">
-				<h1 className="font-semibold text-[36px]">Диалоги</h1>
+				{isDialog ? (
+					<button
+						className="flex items-start h-[16px] ml-3"
+						onClick={() => setIsDialog(false)}
+					>
+						Назад
+					</button>
+				) : (
+					<h1 className="font-semibold text-[36px]">Диалоги</h1>
+				)}
 			</div>
 
-			<div className="grid grid-cols-5 h-[100%]">
-				<div className="col-span-1 flex flex-col gap-1 w-full">
-					<div className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between mr-6  transition-all">
+			<div className="grid grid-cols-1 md:grid-cols-5 h-[100%]">
+				<div
+					className={cn("col-span-1 flex-col gap-1 w-full", {
+						"hidden md:flex": isDialog,
+						flex: !isDialog,
+					})}
+				>
+					<button
+						onClick={() => setIsDialog(true)}
+						className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between md:mr-6  transition-all"
+					>
 						<Image
 							alt=""
 							className="rounded-[20px] z-0 relative min-w-[54px]"
@@ -30,13 +51,16 @@ export default function AccountDialogues() {
 							width={54}
 						/>
 
-						<div className="flex flex-col justify-center text-sm w-full">
+						<div className="flex flex-col justify-center items-start text-sm w-full">
 							<p className="font-semibold">Вероника</p>
 							<p className="-mt-[2px]">35, Москва</p>
 						</div>
-					</div>
+					</button>
 
-					<div className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between mr-6  transition-all">
+					<button
+						onClick={() => setIsDialog(true)}
+						className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between md:mr-6  transition-all"
+					>
 						<Image
 							alt=""
 							className="rounded-[20px] z-0 relative min-w-[54px]"
@@ -46,7 +70,7 @@ export default function AccountDialogues() {
 							width={54}
 						/>
 
-						<div className="flex flex-col justify-center text-sm w-full">
+						<div className="flex flex-col items-start justify-center text-sm w-full">
 							<p className="font-semibold">Кристина</p>
 							<p className="-mt-[2px]">27, Москва</p>
 						</div>
@@ -54,9 +78,12 @@ export default function AccountDialogues() {
 						<div className="w-5 h-5 min-w-5 rounded-full bg-primary flex items-center justify-center mt-1.5 mr-1.5">
 							<p className="text-[10px] font-semibold text-white">2</p>
 						</div>
-					</div>
+					</button>
 
-					<div className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between rounded-r-none  transition-all">
+					<button
+						onClick={() => setIsDialog(true)}
+						className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between md:rounded-r-none  transition-all"
+					>
 						<Image
 							alt=""
 							className="rounded-[20px] z-0 relative min-w-[54px]"
@@ -66,13 +93,16 @@ export default function AccountDialogues() {
 							width={54}
 						/>
 
-						<div className="flex flex-col justify-center text-sm w-full">
+						<div className="items-start flex flex-col justify-center text-sm w-full">
 							<p className="font-semibold">Ольга</p>
 							<p className="-mt-[2px]">27, Москва</p>
 						</div>
-					</div>
+					</button>
 
-					<div className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between mr-6 transition-all">
+					<button
+						onClick={() => setIsDialog(true)}
+						className="h-[60px] flex gap-2.5 rounded-[24px] bg-white dark:bg-foreground-100 p-[3px] justify-between md:mr-6 transition-all"
+					>
 						<Image
 							alt=""
 							className="rounded-[20px] z-0 relative min-w-[54px]"
@@ -82,14 +112,21 @@ export default function AccountDialogues() {
 							width={54}
 						/>
 
-						<div className="flex flex-col justify-center text-sm w-full">
+						<div className="flex flex-col items-start justify-center text-sm w-full">
 							<p className="font-semibold">Вероника</p>
 							<p className="-mt-[2px]">27, Москва</p>
 						</div>
-					</div>
+					</button>
 				</div>
 
-				<div className="col-span-4 border-[7px] p-3 border-white dark:border-foreground-100 w-full h-[70%] rounded-[36px] relative overflow-y-scroll hide-scroll text-[14px]">
+				<div
+					className={cn(
+						"col-span-1 md:col-span-4 border-[7px] p-3 border-white dark:border-foreground-100 w-full h-[70%] rounded-[36px] relative overflow-y-scroll hide-scroll text-[14px]",
+						{
+							"hidden md:flex": !isDialog,
+						}
+					)}
+				>
 					<div className="flex flex-col gap-3">
 						<div className="flex flex-col gap-3 items-end">
 							<div className="flex items-center gap-2">
@@ -105,12 +142,12 @@ export default function AccountDialogues() {
 							</div>
 
 							<div className="flex flex-col justify-between bg-white dark:bg-foreground-100 rounded-[24px] rounded-tr-none px-5 p-3">
-                <p >Привет! Встретимся сегодня?</p>
-                <p className="text-[10px] text-right mt-1">22:30</p>
+								<p>Привет! Встретимся сегодня?</p>
+								<p className="text-[10px] text-right mt-1">22:30</p>
 							</div>
 						</div>
 
-            <div className="flex flex-col gap-3 items-start">
+						<div className="flex flex-col gap-3 items-start">
 							<div className="flex items-center gap-2">
 								<Image
 									alt=""
@@ -125,10 +162,10 @@ export default function AccountDialogues() {
 
 							<div className="flex flex-col justify-between bg-white dark:bg-foreground-100 rounded-[24px] rounded-tl-none px-5 p-3">
 								<p>Да, конечно)</p>
-                <p className="text-[10px] text-right mt-1">22:35</p>
+								<p className="text-[10px] text-right mt-1">22:35</p>
 							</div>
 						</div>
-            <div className="flex flex-col gap-3 items-start">
+						<div className="flex flex-col gap-3 items-start">
 							{/* <div className="flex items-center gap-2">
 								<Image
 									alt=""
@@ -143,7 +180,7 @@ export default function AccountDialogues() {
 
 							<div className="flex flex-col justify-between bg-white dark:bg-foreground-100 rounded-[24px] rounded-tl-none px-5 p-3">
 								<p>Во сколько ты заедешь?</p>
-                <p className="text-[10px] text-right mt-1">22:36</p>
+								<p className="text-[10px] text-right mt-1">22:36</p>
 							</div>
 						</div>
 					</div>
